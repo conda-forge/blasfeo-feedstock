f211593aa5dd4e61eaab0a9f98ef999f8e003e73
Roland Schwan <roland.schwan@mikrounix.com>
2024-11-19 16:28:22 +0100

    disable examples and testing on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,7 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_TESTING:BOOL=ON ^
     -DBLASFEO_TESTING:BOOL=OFF ^
+    -DBLASFEO_EXAMPLES:BOOL=OFF ^
     -DBUILD_SHARED_LIBS:BOOL=ON ^
     -DTARGET=%BLASFEO_TARGET% ^
     %SRC_DIR%
@@ -23,5 +24,5 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Test.
-if errorlevel 1 exit 1
-ctest --output-on-failure -C Release
+:: if errorlevel 1 exit 1
+:: ctest --output-on-failure -C Release
