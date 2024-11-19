0dc34a40c4acf37bea6b070ac7e5f163ceb0a58e
Roland Schwan <roland.schwan@mikrounix.com>
2024-11-19 12:08:38 +0100

    enable microarch for all platforms and specialized builds for arm

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,15 @@ rmdir /s /q build
 mkdir build
 cd build
 
-:: Only GENERIC Target is supported on MSVC
-:: See https://github.com/giaf/blasfeo/blob/b6bc34c6cb1995aea60968403016110bd3288e3b/CMakeLists.txt#L255
 cmake ^
     -G "Ninja" ^
+    -DCMAKE_C_COMPILER=clang-cl ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_TESTING:BOOL=ON ^
-    -DBLASFEO_TESTING:BOOL=ON ^
+    -DBLASFEO_TESTING:BOOL=OFF ^
     -DBUILD_SHARED_LIBS:BOOL=ON ^
-    -DTARGET=GENERIC ^
+    -DTARGET=%BLASFEO_TARGET% ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
